c8438c7e3cf12eaaa2b181551ae4128e5d358c11
Alexandre Guz <guzalexandre@gmail.com>
2024-06-06 13:03:01 +0300

    about page with tailwind

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,14 +1,19 @@
 export default function About() {
   return (
     <>
-      <div>We will be the future of law for you today</div>
+    <div className="w-4/5 mr-auto ml-auto text-center font-bold text-3xl flex flex-col bg-cover min-h-screen bg-[url('../public/hammer.jpg')] justify-between ">
+      <div className="w-4/5 mr-auto ml-auto text-blue-400 text-center font-bold text-3xl mb-10 mt-7">
+        The future of law now for you!
+      </div>
 
-      <h1>
+      <div className="mb-14 text-center">
         We are revolutionizing the efficiency of legal services. Our goal is not
         only to simplify and speed up legal processes through automation, but
         also to spread awareness among legal professionals about the
         possibilities and benefits.
-      </h1>
+      </div>
+    </div>
+
     </>
   );
 }
